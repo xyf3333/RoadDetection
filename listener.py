16a317058a03380a34973552c3dcfdf77f9a6198
Yangfan Xu <34250620+xyf3333@users.noreply.github.com>
2021-10-14 11:50:20 -0400

    Update

--- listener.py
+++ listener.py
@@ -9,45 +9,81 @@ Created on Sat Sep  4 15:23:44 2021
 
 from adafruit_servokit import ServoKit
 import rospy
-from std_msgs.msg import Int32MultiA
+import sensor_msgs.msg
+from sensor_msgs.msg import LaserScan
+from std_msgs.msg import Float32MultiArray
+import message_filters
+import time
 
             
-kit = ServoKit(channels = 16)
+kit  = ServoKit(channels = 16)
 
-def callback(data):
-    """
-    using data information to decide following actions.
-    :param arr: a array data.
-    """
-
-    rospy.loginfo(data.data)
-
-    left = data.data[0]
-    centr = data.data[1]
-    right = data.data[2]
-
-
-    if centr == 0:
+def callback(msg1, msg2):
+    print(msg1)
+    
+    check_road = msg1.data[0]
+    check_centr = msg1.data[1]
+    angle_1 = msg1.data[2]
+    angle_2 = msg1.data[3]
+    stop_sign = msg1.data[4]
+    right_sign = msg1.data[5]
+    left_min = min(msg2.ranges[45:90])
+    top_centr_min = min(min(msg2.ranges[0:45]),min(msg2.ranges[315:359]))
+    right_min = min(msg2.ranges[270:315])
+    centr_2 = min(min(msg2.ranges[0:15]),min(msg2.ranges[335:359]))
+    left = min(msg2.ranges[45:75])
+    right =min(msg2.ranges[295:315])
+    #emergency stop
+    if left_min < 0.15 or right_min < 0.15 or top_centr_min < 0.25:
         kit.continuous_servo[11].throttle = 0
+    #stop sign
+    if stop_sign == 1.0:
+        #print("stop sign detected_______________________________________________________")
+        
+        kit.continuous_servo[11].throttle = 0
+        kit.servo[0].angle = 90
+           
     else:
+       
         kit.continuous_servo[11].throttle = 0.3
+        kit.servo[0].angle = 90
+        #avoid hit the wall
+        if left_min < 0.3:
+            kit.servo[0].angle = 100
+        if right_min < 0.3:
+            kit.servo[0].angle = 80
 
-    if left > 0:
-        kit.servo[0].angle = 65
-    if right > 0:
-        kit.servo[0].angle = 115
+        if centr_2 < 2.0:
+        #turn left
+            if right_min < 0.25 or left > 0.6:
+                kit.servo[0].angle = 45
+         
+        #turn right
+            if  left_min < 0.25 or right > 0.6:
+                kit.servo[0].angle = 135
+        #stop
+            if right_min < 0.3 and left_min < 0.3:
+                kit.continuous_servo[11].throttle = 0  
 
-    if left > right:
-        kit.servo[0].angle = 65
-    else:
-        kit.servo[0].angle = 115
+        elif check_road == 1.0:
+            if check_centr == -1.0:
+                kit.servo[0].angle = 80
+            elif check_centr == 1.0:
+                kit.servo[0].angle = 100
+            else:
+                kit.servo[0].angle = 90
+ 
+def listener():
     
 
+    rospy.init_node('listener', anonymous = True)
 
-def listener():
+    print("init node")
+    cam_sub = message_filters.Subscriber('cam_values', Float32MultiArray)
+    lidar_sub = message_filters.Subscriber('/scan', LaserScan)
+    ts = message_filters.ApproximateTimeSynchronizer([cam_sub, lidar_sub], queue_size = 10, slop = 0.1, allow_headerless = True)
+    ts.registerCallback(callback)
 
-    rospy.init_node('listener', anonymous=True)
-    rospy.Subscriber('cam_values', Int32MultiArray, callback)
     rospy.spin()
 
 
--- talker.py
+++ talker.py
@@ -5,120 +5,326 @@ Created on Sun Sep  5 09:52:57 2021
 
 @author: py
 """
+import os
+import sys
 import cv2
 import numpy as np
 import rospy
-from std_msgs.msg import Int32MultiArray
+from std_msgs.msg import Float32MultiArray
+import math
+from sensor_msgs.msg import LaserScan
+
+
+try:
+    import tensorflow as tf
+except ImportError:
+    print("unable to import TensorFlow. Is it installed?")
+    sys.exit(1)
+
+# Object detection module imports
+import object_detection
+from object_detection.utils import label_map_util
+from object_detection.utils import visualization_utils as vis_util
+
+#Set model here:
+MODEL_PATH = os.path.join(os.path.dirname(sys.path[0]),'/home/py/catkin_ws/src/transferlearning_course/transferlearning_course/models/research/object_detection/course_model_1')
+PATH_TO_CKPT = MODEL_PATH + '/frozen_inference_graph.pb'
+
+#Set the label map file here:
+LABEL_NAME = 'course.pbtxt'
+PATH_TO_LABELS = os.path.join(os.path.dirname(sys.path[0]),'/home/py/catkin_ws/src/transferlearning_course/transferlearning_course/models/research/object_detection/data', LABEL_NAME)
+
+#Set the number of classes here
+NUM_CLASSES = 2
+
+detection_graph = tf.Graph()
+
+with detection_graph.as_default():
+    od_graph_def = tf.compat.v1.GraphDef()
+    with tf.io.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
+        serialized_graph = fid.read()
+        od_graph_def.ParseFromString(serialized_graph)
+        tf.import_graph_def(od_graph_def, name='')
+
+#Loading label map
+label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
+categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
+category_index = label_map_util.create_category_index(categories)
+
+# Setting the GPU options to use fraction of gpu that has been set
+config = tf.ConfigProto()
+config.gpu_options.per_process_gpu_memory_fraction=0.5
+
+'''
+#Used when testing on saved images: 
+PATH_TO_TEST_IMAGES_DIR = 'object_detection'
+TEST_IMAGE_PATHS = [ os.path.join(PATH_TO_TEST_IMAGES_DIR, 'image{}.jpg'.format(i)) for i in range(0, 501) ]
+'''
+
+class Detector:
+    def __init__(self):
+        #tensorflow session is initiated
+        self.i = 0
+        self.sess = tf.Session(graph=detection_graph,config=config)
+        #self.throttle = 0
+
+    def pred(self, image):
+        height, width, depth = image.shape
+        area = height*width
+        aplha = 0.4
+       
+        #Low lightning conditions:
+        overlay = image.copy()#rgb 
+        output = image#rgb
+        cv2.rectangle(overlay, (0, 0), (width, height),(246, 245, 255), -1)
+        cv2.addWeighted(overlay, aplha, output, 1 - aplha,0, output)
+
+        output=cv2.cvtColor(output, cv2.COLOR_BGR2RGB)#BGR
+        cam_np = np.asarray(output)#has to be rgb
+        cam_np_expanded = np.expand_dims(cam_np, axis=0)
+
+        image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
+        boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
+        scores = detection_graph.get_tensor_by_name('detection_scores:0')
+        classes = detection_graph.get_tensor_by_name('detection_classes:0')
+        num_detections = detection_graph.get_tensor_by_name('num_detections:0')
+        
+        (boxes, scores, classes, num_detections) = self.sess.run([boxes, scores, classes, num_detections],
+            feed_dict={image_tensor: cam_np_expanded})
+        B = np.squeeze(boxes)
+        S = np.squeeze(scores)
+        C = np. squeeze(classes)
+        #print ("Classes:",C[0])
+        #print ("Scores:",S)
+        if scores[0][0] >= 0.5:
+            cam_box1 = boxes[0][0].tolist()
+        else:
+            cam_box1 = [0,0,0,0]
+
+
+
+        cam_ymin1, cam_xmin1, cam_ymax1, cam_xmax1 = cam_box1
+        cam_x_center_box1 = int(cam_xmin1 * width + (cam_xmax1 - cam_xmin1) * width * 0.5)
+        cam_y_center_box1 = int(cam_ymin1 * height + (cam_ymax1 - cam_ymin1) * height * 0.5)
+        size1 = abs(cam_xmax1-cam_xmin1) * abs(cam_ymax1-cam_ymin1)
+        size1 = 100*size1/area
+
+        objects=vis_util.visualize_boxes_and_labels_on_image_array(
+            output,
+            np.squeeze(boxes),
+            np.squeeze(classes).astype(np.int32),
+            np.squeeze(scores),
+            category_index,
+            use_normalized_coordinates=True,
+            line_thickness=2)
+        
+        img=cv2.cvtColor(output, cv2.COLOR_RGB2BGR)  
+        cv2.resize("sign_detection", (640, 480))
+        cv2.imshow("sign_detection", img)
+
+        cv2.waitKey(3)
+
+        return C, S, B
+
+
+
 
 
 def check_zero_in_matrix(arr):
     """
     Check if all 2D numpy array contains only 0
     :param arr: a 2D array
-    :return: if all elements are 0, return 0, if any element is non-zero, return 1
+    :return: if all elements are 0, return False, if anyone is non-zero, return True
     """
     result = np.all((arr == 0))
     if result:
-        return 0
+        return False
     else:
-        return 1
+        return True
 
 
 def region_of_interest(img, vertices):
-
-    """
-    Cropping the image using vertices
-    :param arr: image, vertices of interest area
-    :return: cropped image only represent interest region
-    """
-
     mask = np.zeros_like(img)
     match_mask_color = 255
     cv2.fillPoly(mask, vertices, match_mask_color)
-    cropped_image = cv2.bitwise_and(img, mask)
-    return cropped_image
+    masked_image = cv2.bitwise_and(img, mask)
+    return masked_image
 
+def vertical_angle(img):
+
+    angle = [500.0,500.0]
+    if check_zero_in_matrix(img) == False:
+
+        return angle
+
+    top_points = [float("inf"),float("inf")]
+    bottom_points = [float("inf"),float("inf")]
+    row = img.shape[0]
+    col = img.shape[1]
+
+    bottom_array = img[[row-1],:][0]
+    top_array = img[[int(2*row/3)],:][0]
+    
+
+
+    #top_left
+    for i in range(len(top_array)):
+        
+        if top_array[i-1] == 255:
+            top_points[0] = i-1
+            break
+    #top_right
+    for i in range(len(top_array), -1, -1):
+        if top_array[i-1] ==255:
+            top_points[1] = i-1
+            break
+    #bottom_left 
+    for i in range(len(bottom_array)):
+        if bottom_array[i-1] == 255:
+            bottom_points[0] = i-1
+            break
+    #bottom_right
+    for i in range(len(bottom_array), -1, -1):
+        if bottom_array[i-1] == 255:
+            bottom_points[1] = i-1
+            break
+
+    #print("----------------------------------------------")
+    #print("top_points", top_points)
+    #print("bottom_points", bottom_points)
+
+
+    bottom_centr = float(bottom_points[0] + bottom_points[1])/2.0
+    diff_vertical = float(col/3)
+
+    dis_top_points = top_points[1] - top_points[0]
+    dis_bottom_points = bottom_points[1] -  bottom_points[0]
+
+    if bottom_points[0] != float("inf") and bottom_points[1] != float("inf") and  top_points[0] != float("inf") and top_points[1] != float("inf") and dis_bottom_points != 0:
+        if top_points[1] < bottom_centr:
+            angle[0] = (-1)*math.degrees(math.atan(float(abs(top_points[0]- bottom_centr)/diff_vertical)))
+            angle[1] = (-1)*math.degrees(math.atan(float(abs(top_points[1]- bottom_centr)/diff_vertical)))
+        elif top_points[0] > bottom_centr:
+            angle[0] = math.degrees(math.atan(float(abs(top_points[0]- bottom_centr)/diff_vertical)))
+            angle[1] = math.degrees(math.atan(float(abs(top_points[1]- bottom_centr)/diff_vertical)))
+        else:
+            angle[0] = (-1)*math.degrees(math.atan(float(abs(top_points[0]- bottom_centr)/diff_vertical)))
+            angle[1] = math.degrees(math.atan(float(abs(top_points[1]- bottom_centr)/diff_vertical)))
+
+
+    return angle
+
+def keep_centr(img,points_of_interest):
+    row = img.shape[0]
+    col = img.shape[1]
+
+    line = row/6
+    bottom_array = img[[row - 1],:][0]
+
+    bottom_points_index = [-1,-1]
+ 
+    #bottom_left 
+    for i in range(len(bottom_array)):
+        if bottom_array[i-1] != 0:
+            #print("first non_0")
+            bottom_points_index[0] = i-1
+    #bottom_right
+    for i in range(len(bottom_array), -1, -1):
+        if bottom_array[i-1] != 0:
+            #print("second non_0")
+            bottom_points_index[1] = i-1
+
+
+    centr_point  =  float(bottom_points_index[0]+ bottom_points_index[1])/2.0
+   
+    if centr_point < 3.0*float(col)/8.0:
+        return -1.0
+    if centr_point > 5.0*float(col)/8.0:
+        return 1.0
+    return 0.0
 
 
 def process(image):
-
-    """
-    processing image data
-    :param arr: image
-    :return: if all elements are 0, return 0, if any element is non-zero, return 1
-    """
-
-
-    wide = image.shape[0]
-    height = image.shape[1]
-
-    # definite the interest area
+    row = image.shape[0]
+    col = image.shape[1]
+    
     region_of_interest_vertices_centr_botton = [
-        (wide/8, height ),
-        (wide/8,height/3),
-        (7*wide/8, height/3),
-        (7*wide/8, height)
+        (0, row),
+        (0, row/3),
+        (col, row/3),
+        (col-1, row-1)
     
     ]
-    region_of_interest_vertices_left = [
-        (0, height),
-        (0,height/3),
-        (wide/3, height/3),
-        (wide/3,height)
-    ]
-    region_of_interest_vertices_right = [
-        (2*wide/3, height),
-        (2*wide/3, height/3),
-        (wide, height/3),
-        (wide, height)
-    ]
 
-    #image data conversion
-
-    #detect yellow road trail using HSV color boundary
+    
     hsv_frame = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
     l_b = np.array([17, 92, 137])
-    u_b = np.array([50, 255, 255])
+    u_b = np.array([60, 255, 255])
     mask = cv2.inRange(hsv_frame, l_b, u_b)
+    cv2.imshow("Frame", image)
+    cv2.resizeWindow('Frame', (640, 480)
+
     res = cv2.bitwise_and(image, image, mask= mask)
-
-    #transfer to gray image
     gray_image = cv2.cvtColor(res, cv2.COLOR_RGB2GRAY)
-
-    #detect edge of road
+    
     canny_image = cv2.Canny(gray_image, 100, 200)
-
+    
     cropped_image_centr_botton = region_of_interest(canny_image,
                     np.array([region_of_interest_vertices_centr_botton], np.int32))
-    
-    cropped_image_right = region_of_interest(canny_image,
-                    np.array([region_of_interest_vertices_right], np.int32))
-    
-    cropped_image_left = region_of_interest(canny_image,
-                    np.array([region_of_interest_vertices_left], np.int32))
+
+  
+    if check_zero_in_matrix(cropped_image_centr_botton):
+        check_road = 1.0
+    else:
+        check_road = 0.0
+
+    centr_check = 0.0
+    if check_road == 1.0:
+        angle_list= vertical_angle(cropped_image_centr_botton)
+        centr_check = keep_centr(cropped_image_centr_botton, region_of_interest_vertices_centr_botton) 
+    else:
+        angle_list = [500.0,500.0]
 
 
-    centr = check_zero_in_matrix(cropped_image_centr_botton)
-    left_sum = cropped_image_left.sum()                                 #the sum of pixel value
-    right_sum = cropped_image_right.sum()                               #the sum of pixel value
-
-    return [left_sum, centr, right_sum]
+    return check_road, centr_check, round(angle_list[0],1), round(angle_list[1],1)
 
 def talker():
-
-    rospy.init_node('talker', anonymous=True)                                               #initial publisher node
-    data_publisher = rospy.Publisher('cam_values', Int32MultiArray, queue_size= 10)         #define the publisher data
-    rate = rospy.Rate(10)
+    data_publisher = rospy.Publisher('cam_values', Float32MultiArray, queue_size = 20)
+    rospy.init_node('talker', anonymous = True)
+    rate_1 = rospy.Rate(60)
     cap = cv2.VideoCapture(0)
+    a = Detector()
+    flag = 0
+    stop = 0.0
+    right = 0.0
     while not rospy.is_shutdown():
         while cap.isOpened():
             ret, frame = cap.read()
-            cv2.imshow('vv',frame)
-            msg = Int32MultiArray()
-            msg.data = process(frame) 
+            msg = Float32MultiArray()
+            mag_temp = process(frame)
+            frame_tran = cv2.resize(frame,(640, 480),fx = 0,fy = 0,interpolation = cv2.INTER_CUBIC)
+            gray = cv2.cvtColor(frame_tran, cv2.COLOR_BGR2GRAY)
+            frame_tran[:,:,0] = gray
+            frame_tran[:,:,1] = gray
+            frame_tran[:,:,2] = gray
+            x,y,z = a.pred(frame_tran) #x:classes, y:scores
+            mag_temp = list(mag_temp)
+            if flag <= 180 and flag > 0  :
+                flag = flag - 1
+                stop = 0.0
+            elif flag > 180 :
+                flag = flag - 1
+                stop = 1.0
+            if flag == 0:
+                if x[0] == 1.0 and y[0] > 0.7:
+                    stop = 1.0
+                    flag = 360
+
+            mag_temp.extend([stop,right])
+            msg.data = mag_temp
             rospy.loginfo(msg)
             data_publisher.publish(msg)
-            rate.sleep()
+            rate_1.sleep()
 
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
@@ -131,4 +337,3 @@ if __name__=='__main__':
         talker()
     except rospy.ROSInterruptException:
         pass
-    
